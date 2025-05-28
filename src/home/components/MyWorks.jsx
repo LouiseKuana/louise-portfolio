2a891fc2fd60a955e6780447294939e0a66f21e5
LouiseAkemi <lou.kuana@hotmail.com>
2025-05-28 16:27:28 -0500

    style: adjusting coming soon projects

--- src/home/components/MyWorks.jsx
+++ src/home/components/MyWorks.jsx
@@ -1,29 +1,36 @@
-import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "../styles/MyWorks.css";
+import { EggCrack, BookOpen, Warning } from "phosphor-react";
 
 export default function MyWorks() {
   const works = [
     {
-      title: "🐱 Portfolio Fofinho",
-      description: "Um portfólio super kawaii com cores pastel.",
-      image: "/src/assets/petals.png",
-      link: "https://meuportfolio.com",
+      icon: (
+        <EggCrack className="text-fuchsia-rose mx-1 text-[min(4vw,16px)]" />
+      ),
+      title: "Coming soon",
+      description: "Just a little patience — I'm working on it.",
+      image: "src/assets/lionFlower.gif",
+      isAvailable: false,
     },
     {
-      title: "🍓 Loja de Morangos",
-      description: "E-commerce temático de doces.",
-      image: "/src/assets/petals.png",
-      link: "https://lojadomorango.com",
+      icon: (
+        <BookOpen className="text-fuchsia-rose mx-1 text-[min(4vw,16px)]" />
+      ),
+      title: "Coming soon",
+      description: "Just a little patience — I'm working on it.",
+      image: "src/assets/lionFlower.gif",
+      isAvailable: false,
     },
     {
-      title: "🌸 App Sakura",
-      description: "Aplicativo para acompanhar flores de cerejeira.",
-      image: "/src/assets/petals.png",
-      link: "https://appsakura.com",
+      icon: <Warning className="text-fuchsia-rose mx-1 text-[min(4vw,16px)]" />,
+      title: "Coming soon",
+      description: "Just a little patience — I'm working on it.",
+      image: "src/assets/lionFlower.gif",
+      isAvailable: false,
     },
   ];
 
@@ -47,32 +54,52 @@ export default function MyWorks() {
       >
         {works.map((work, index) => (
           <SwiperSlide key={index} className="flex justify-center items-center">
-            <a
-              href={work.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="w-48 md:w-80 aspect-[3/4] bg-lavender-blush border-4 border-amaranth-pink rounded-3xl p-3 hover:shadow-pink-300 transition-all duration-300 flex flex-col items-center mx-auto"
-            >
-              <img
-                src={work.image}
-                alt={work.title}
-                className="w-full h-auto object-cover rounded-2xl mb-2 shadow-md"
-              />
-              <h3 className="text-lg font-bold text-fuchsia-rose font-kawaii mb-1 text-center">
-                {work.title}
-              </h3>
-              <p className="text-fuchsia-rose text-xs text-center">
-                {work.description}
-              </p>
-            </a>
+            {work.isAvailable ? (
+              <a
+                href={work.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-[min(45vw,320px)] aspect-[3/4] bg-lavender-blush border-4 border-amaranth-pink rounded-3xl p-3 hover:shadow-pink-300 transition-all duration-300 flex flex-col items-center mx-auto"
+              >
+                <img
+                  src={work.image}
+                  alt={work.title}
+                  className="w-full h-auto object-cover rounded-2xl mb-2 shadow-md"
+                />
+                <h3 className="text-base md:text-lg font-bold text-fuchsia-rose mb-1 text-center">
+                  <span className="inline-flex items-center align-middle ml-1">
+                    {work.icon}
+                  </span>
+                  {work.title}
+                </h3>
+                <p className="text-fuchsia-rose text-xs text-center">
+                  {work.description}
+                </p>
+              </a>
+            ) : (
+              <div className="w-[min(45vw,320px)] aspect-[3/4] bg-lavender-blush border-4 border-amaranth-pink rounded-3xl p-3 flex flex-col items-center mx-auto opacity-60 cursor-not-allowed">
+                <img
+                  src={work.image}
+                  alt={work.title}
+                  className="w-full h-auto object-cover rounded-2xl mb-2 shadow-md"
+                />
+                <h3 className="text-base md:text-lg font-bold text-fuchsia-rose mb-1 text-center">
+                  <span className="inline-flex items-center align-middle ml-1">
+                    {work.icon}
+                  </span>
+                  {work.title}
+                </h3>
+                <p className="text-fuchsia-rose text-xs text-center">
+                  {work.description}
+                </p>
+              </div>
+            )}
           </SwiperSlide>
         ))}
 
         {/* Botões de navegação personalizados */}
-        <div className="swiper-button-prev custom-swiper-button-prev">
-        </div>
-        <div className="swiper-button-next custom-swiper-button-next">
-        </div>
+        <div className="swiper-button-prev custom-swiper-button-prev"></div>
+        <div className="swiper-button-next custom-swiper-button-next"></div>
       </Swiper>
     </div>
   );
