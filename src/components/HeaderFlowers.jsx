d79bf403b0b89c056e0a896d94d947028ae7ea7c
LouiseAkemi <lou.kuana@hotmail.com>
2025-05-29 11:33:39 -0500

    refactor: custom img header flowers position

--- src/components/HeaderFlowers.jsx
+++ src/components/HeaderFlowers.jsx
@@ -2,13 +2,13 @@ export default function HeaderFlowers() {
   return (
     <>
       <img
-        className="hidden md:block fixed top-0 left-0 z-0 w-[min(25vw,340px)]"
+        className="custom-flowers-position left-0"
         src="src/assets/petalsAndFlowers.png"
         alt="Petals left side"
       />
 
       <img
-        className="hidden md:block fixed top-0 right-0 z-0 scale-x-[-1] w-[min(25vw,340px)]"
+        className="custom-flowers-position right-0 scale-x-[-1] "
         src="src/assets/petalsAndFlowers.png"
         alt="Petals right side"
       />
